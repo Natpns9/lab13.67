700dda053924b399a531f8b826065c6ed475c7a4
natpns <panisanat19@gmail.com>
2024-12-28 19:55:08 +0700

    Finished

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,31 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]) {
+    double sum = 0, sumSquares = 0, product = 1, harmonicSum = 0;
+    double maxVal = A[0], minVal = A[0];
+
+    for (int i = 0; i < N; i++) {
+        sum += A[i];
+        sumSquares += A[i] * A[i];
+        product *= A[i];
+        harmonicSum += 1.0 / A[i];
+
+        if (A[i] > maxVal) maxVal = A[i];
+        if (A[i] < minVal) minVal = A[i];
+    }
+
+    double mean = sum / N;
+    double variance = (sumSquares / N) - (mean * mean);
+    double stdDev = sqrt(variance);
+    double geoMean = pow(product, 1.0 / N);
+    double harmMean = N / harmonicSum;
+
+    B[0] = mean;       
+    B[1] = stdDev;     
+    B[2] = geoMean;    
+    B[3] = harmMean;   
+    B[4] = maxVal;     
+    B[5] = minVal;    
+}
